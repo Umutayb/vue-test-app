e3a6b2f524bd41ac0469ee0291e81ac10c05b8aa
InnaDrapii <i.drapii@gmail.com>
2024-02-29 12:24:25 +0000

    logo and image

--- src/views/categories/InteractionsPage.vue
+++ src/views/categories/InteractionsPage.vue
@@ -3,17 +3,34 @@
     <h2>Interactions Page</h2>
     <a>Wellcome to Interactions!</a>
     <ToolLayout></ToolLayout>
+    <div id="image">
+        <img :src="image"/>
+    </div>
+    <div id="logo">
+        <img :src="logo"/>
+    </div>
   </div>
 </template>
 
 <script>
 import '@vuepic/vue-datepicker/dist/main.css'
 import ToolLayout from "@/components/tools/ToolLayout.vue";
+import logo from "@/assets/logo.png"
+import image from "@/assets/image.png"
 
 export default {
   components: {
     ToolLayout
   },
+  data: function () {
+            return {
+              logo: logo,
+              image: image
+            }
+        }
  
 }
 </script>
+
+<style lang="css">
+</style>
